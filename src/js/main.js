e28f448159af0135cb34bcc53b631640e21cb69f
Matthijs Hoek <matthijs.hoek@hotmail.nl>
2019-03-22 15:28:11 +0100

    Made a start on gulp

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+const button = document.querySelector('.box__button');
+const text = document.querySelector('.box__text');
+const string1 = 'Hello';
+const string2 = 'There!';
+
+class Box {
+    constructor(button, text, first, second) {
+        this.button = button;
+        this.text = text;
+        this.first = first;
+        this.second = second;
+
+        button.addEventListener('click', () => {this.fillText()});
+
+        first = string1;
+        second = string2;
+
+    }
+
+    fillText() {
+        text.textContent = this.first + " " + this.second ;
+    }
+}
+
+new Box (button, text, string1, string2);

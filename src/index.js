945fc8aed5a6addba63eeaf0f90378483bf8ddde
Matthijs Hoek <matthijs.hoek@hotmail.nl>
2019-03-27 15:30:47 +0100

    Started on webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+import './styles/sass/main.scss';
+
+function component() {
+    let element = document.createElement('div');
+    return element;
+  }
+  
+  document.body.appendChild(component());

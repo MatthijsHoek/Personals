8daf78c2ad109172d131f753cbcc046626562a72
Matthijs Hoek <matthijs.hoek@hotmail.nl>
2019-05-15 16:04:14 +0200

    Fixed sass task, Added uglify and css-extract

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,46 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+    optimization: {
+        minimizer: [new UglifyJsPlugin()],
     },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: '[name].css',
+          chunkFilename: '[id].css',
+        }),
+      ],
+
     module: {
         rules: [{
             test: /\.scss$/,
             use: [
-                "style-loader", // creates style nodes from JS strings
-                "css-loader", // translates CSS into CommonJS
-                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                "style-loader",
+                "css-loader", 
+                "sass-loader" 
             ]
         }]
-    }
+    },
+    
+    module: {
+        rules: [
+        {
+            test: /\.css$/,
+            use: [
+            {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                publicPath: '../',
+                hmr: process.env.NODE_ENV === 'development',
+                },
+            },
+            'css-loader',
+            ],
+        },
+        ],
+    },
+    watch: true
 };
